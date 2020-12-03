99676c6030c4fa465d845dede95bc3cc0a8cb2c3
Konstantin Chuvilyov <f1nzer@outlook.com>
2020-12-03 21:00:00 +0700

    feat(split): added parallel bake processing & code cleanup

--- split.py
+++ split.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python3
 import os
-import sys
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from tempfile import NamedTemporaryFile
 from glob import glob
-from numpy import allclose, array, degrees, identity, math, matrix, sqrt
-from shutil import copyfileobj, move
-
-
-scan_index = 0
-
+from numpy import array, identity, matrix
+from shutil import move
+from multiprocessing import Pool
+from itertools import islice
+from functools import partial
 
 def parse_cmd():
-    """Parse the command line."""
     parser = ArgumentParser(description=__doc__,
                             formatter_class=RawDescriptionHelpFormatter,
-                            usage='%(prog)s {s,split,b,bake} \n'
-                                  '         ptx_file')
+                            usage='%(prog)s {s,split,b,bake} ptx_file')
     parser.add_argument('command',
                         choices=['s', 'split', 'b', 'bake', ],
                         help="Action to perform on input ptx file.")
-    # parser.add_argument('ptx_file', nargs='+')
+    parser.add_argument('ptx_file', nargs='+')
+    parser.add_argument('target_folder')
 
     args = parser.parse_args()
-
-    if args.matrix_file:
-        if len(args.matrix_file) == 1:
-            args.matrix_file = glob(args.matrix_file[0])
-        args.matrix_file.sort()
 
     if len(args.ptx_file) == 1:
         args.ptx_file = glob(args.ptx_file[0])
@@ -101,63 +93,95 @@ def create_header(mat, rows, cols):
             'scanner_position': ptx_ar[3, 0:3].tolist()}
 
 
-def handle_scan(args, ptx_file):
-    print("\nProcessing: %s" % ptx_file)
+def split_file_by_scans(ptx_file: str):
     with open(ptx_file, 'rb') as ptx_handle:
         basename = os.path.splitext(os.path.split(ptx_file)[1])[0]
         scan_num = 0
-        msg = []
-        # EOF has not been encountered.
         while True:
             header_txt = []
             while len(header_txt) < 10:
                 header_txt.append(ptx_handle.readline())
-                if not header_txt[-1]:
-                    break
-                header = parse_ptx_header(header_txt)
-                new_mat = None
-                if args.command in ['s', 'split']:
-                    out_file = '%s_%0.3d.ptx' % (basename, scan_num)
-                    msg.append(("Writing scan file:", out_file))
-                    with open(out_file, 'wb') as out_handle:
-                        out_handle.writelines(list_ptx_header(header))
-                        copy_data(ptx_handle, out_handle, header['n_points'])
-                elif args.command in ['b', 'bake']:
-                    identity_header = create_header(identity(4),
-                                                    header['rows'],
-                                                    header['columns'])
-                    with NamedTemporaryFile(delete=False) as tmp_handle:
-                        tmp_handle.writelines(list_ptx_header(identity_header))
-                        percent = .1
-                        for nn in range(header['n_points']):
-                            if nn == int(header['n_points'] * percent):
-                                print('...%i%%' % int(percent * 100), end="")
-                                percent += .1
-                            line = ptx_handle.readline().split()
-                            coords = [float(v) for v in line[:3]]
+            if not header_txt[-1]:
+                break
 
-                            if coords[0] != 0 and coords[1] != 0 and coords[2] != 0:
-                                coords.append(1)
-                                coords = matrix(coords)
-                                coords = header['matrix'].dot(
-                                    coords.T).T.tolist()
-                                res_out = b'%s %s\n' % (b'%.5f %.5f %.5f' % tuple(
-                                    coords[0][:3]), b' '.join(line[3:]))
-                            else:
-                                res_out = b'%s\n' % b' '.join(line)
-
-                            tmp_handle.write(res_out)
-                        print()
-
-                    move(tmp_handle.name, '%s_%0.3d.ptx' %
-                         (basename, scan_num))
+            header = parse_ptx_header(header_txt)
+            out_file = '%s_%0.3d.ptx' % (basename, scan_num)
+            with open(out_file, 'wb') as out_handle:
+                out_handle.writelines(list_ptx_header(header))
+                copy_data(ptx_handle, out_handle, header['n_points'])
 
             scan_num += 1
 
 
+def bake_line(header, raw_line: str):
+    line = raw_line.split()
+    coords = [float(v) for v in line[:3]]
+
+    if (coords[0] == coords[1] == coords[2] == 0):
+        return raw_line
+
+    coords.append(1)
+    coords = matrix(coords)
+    coords = header['matrix'].dot(coords.T).T.tolist()
+    return b'%s %s\n' % (b'%.5f %.5f %.5f' % tuple(coords[0][:3]), b' '.join(line[3:]))
+    
+
+def bake_file_by_scans(ptx_file: str, target_folder: str):
+    print(ptx_file)
+    with open(ptx_file, 'rb') as ptx_handle:
+        basename = os.path.splitext(os.path.split(ptx_file)[1])[0]
+        scan_num = 0
+        while True:
+            header_txt = []
+            while len(header_txt) < 10:
+                header_txt.append(ptx_handle.readline())
+            if not header_txt[-1]:
+                break
+
+            header = parse_ptx_header(header_txt)
+            func = partial(bake_line, header)
+
+            identity_header = create_header(identity(4), header['rows'], header['columns'])
+            with NamedTemporaryFile(delete=False) as tmp_handle:
+                tmp_handle.writelines(list_ptx_header(identity_header))
+                total_points = int(header['n_points'])
+                done_points = 0
+                while True:
+                    progress = round(done_points / total_points * 100)
+                    print(f'Scan {scan_num:03d}: {done_points} / {total_points} | {progress}%')
+
+                    batch_size = min(10_000_000, total_points - done_points)
+                    next_n_lines = list(islice(ptx_handle, batch_size))
+                    if not next_n_lines:
+                        break
+                    
+                    with Pool() as pool:
+                        result = pool.map(func, next_n_lines)
+
+                    tmp_handle.writelines(result)
+                    done_points += len(next_n_lines)
+
+                print()
+
+            target_baked_file = '%s_%0.3d.ptx' % (basename, scan_num)
+            target_baked_filename = os.path.join(target_folder, target_baked_file)
+            move(tmp_handle.name, target_baked_filename)
+
+            scan_num += 1
+
+
+def handle_scan(command: str, ptx_file: str, target_folder: str):
+    print("\nProcessing: %s" % ptx_file)
+    if command in ['s', 'split']:
+        split_file_by_scans(ptx_file, target_folder)
+    elif command in ['b', 'bake']:
+        bake_file_by_scans(ptx_file, target_folder)
+
+
 def main():
+    print('priv')
     args = parse_cmd()
-    handle_scan(args, args.ptx_file)
+    handle_scan(args.command, args.ptx_file[0], args.target_folder)
 
 
 if __name__ == '__main__':
